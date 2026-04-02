7f565d09c6b11f6de9688c79776e14fce25e78c3
kanghyein75 <kanghye675@gmail.com>
2026-04-02 23:39:26 +0900

    Update 2-4.c

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -8,6 +8,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         char ch = chars[i];
         printf("%c - alpha:%d digit:%d upper:%c lower:%c\n",
+            ch,
             isalpha(ch),  
             isdigit(ch),  
             toupper(ch),  
