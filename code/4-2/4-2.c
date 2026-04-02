0c7c9c72a3279d01a62799cc0701add451986598
kanghyein75 <kanghye675@gmail.com>
2026-04-02 23:24:20 +0900

    Update 4-2.c

--- code/4-2/4-2.c
+++ code/4-2/4-2.c
@@ -13,7 +13,7 @@ double average(int arr[], int n) {
 }
 
 int max(int arr[], int n) {
-    int m = arr[0]; 
+    int m = 0; 
     for (int i = 1; i < n; i++) {
         if (arr[i] > m) {
             m = arr[i];
